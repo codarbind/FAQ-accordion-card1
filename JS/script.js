88960ceb1cefde7ff1e8c067b13a440793988b0e
seunawonugba@gmail.com <59831680+seunawonugba@users.noreply.github.com>
2020-12-02 02:59:54 +0100

    version1

---
+++ JS/script.js
@@ -0,0 +1,54 @@
+function toggleFunction0(){
+    let x=document.getElementById("answer0");
+    if(x.style.display === "none" ){
+        x.style.display = "block";
+    }
+
+    else{
+        x.style.display = "none";
+    }
+}
+
+function toggleFunction1(){
+    let x=document.getElementById("answer1");
+    if(x.style.display === "none" ){
+        x.style.display = "block";
+    }
+
+    else{
+        x.style.display = "none";
+    }
+}
+
+function toggleFunction2(){
+    let x=document.getElementById("answer2");
+    if(x.style.display === "none" ){
+        x.style.display = "block";
+    }
+
+    else{
+        x.style.display = "none";
+    }
+}
+
+function toggleFunction3(){
+    let x=document.getElementById("answer3");
+    if(x.style.display === "none" ){
+        x.style.display = "block";
+    }
+
+    else{
+        x.style.display = "none";
+    }
+}
+
+function toggleFunction4(){
+    let x=document.getElementById("answer4");
+    if(x.style.display === "none" ){
+        x.style.display = "block";
+    }
+
+    else{
+        x.style.display = "none";
+    }
+}
