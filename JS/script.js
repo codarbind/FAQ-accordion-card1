0ed3344e1391cfa2482e53c61ab2fc0d43ccacc7
ADEKOLA, Abdwahab <adekolaabdwahababisoye@gmail.com>
2020-12-02 10:01:53 +0100

    one function is now used for all

--- JS/script.js
+++ JS/script.js
@@ -1,5 +1,5 @@
-function toggleFunction0(){
-    let x=document.getElementById("answer0");
+function toggleFunction(clickedId){
+    let x=document.getElementById("answer"+clickedId);
     if(x.style.display === "none" ){
         x.style.display = "block";
     }
@@ -9,46 +9,3 @@ function toggleFunction0(){
     }
 }
 
-function toggleFunction1(){
-    let x=document.getElementById("answer1");
-    if(x.style.display === "none" ){
-        x.style.display = "block";
-    }
-
-    else{
-        x.style.display = "none";
-    }
-}
-
-function toggleFunction2(){
-    let x=document.getElementById("answer2");
-    if(x.style.display === "none" ){
-        x.style.display = "block";
-    }
-
-    else{
-        x.style.display = "none";
-    }
-}
-
-function toggleFunction3(){
-    let x=document.getElementById("answer3");
-    if(x.style.display === "none" ){
-        x.style.display = "block";
-    }
-
-    else{
-        x.style.display = "none";
-    }
-}
-
-function toggleFunction4(){
-    let x=document.getElementById("answer4");
-    if(x.style.display === "none" ){
-        x.style.display = "block";
-    }
-
-    else{
-        x.style.display = "none";
-    }
-}
